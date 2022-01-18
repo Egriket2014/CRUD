a02012347b8c7795b62d3e5f777f8c2cf4f7c100
Egriket <egriket2014@yandex.ru>
2022-01-18 16:21:53 +0300

    L23 Add Update and Delete functions

--- src/main/java/mvcapp/dao/PersonDAO.java
+++ src/main/java/mvcapp/dao/PersonDAO.java
@@ -31,4 +31,13 @@ public class PersonDAO {
         person.setId(++PEOPLE_ID);
         people.add(person);
     }
+
+    public void update(int id, Person person) {
+        Person updatePerson = show(id);
+        updatePerson.setName(person.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
 }
